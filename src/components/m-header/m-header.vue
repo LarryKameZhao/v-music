d12658c8d9be8dfc635fca975a38f59e08d2f25d
LarryZhao <larryzhao1994@outlook.com>
2019-03-05 00:33:10 +0800

    add m-header components

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="m-header">
+        <div class="icon"></div>
+        <div class="text">tecent Music</div>
+            <i class="icon-mine"></i>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "m-header"
+  }
+</script>
+
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+    .m-header{
+        position: relative;
+        height: 44px;
+        text-align: center;
+        color: $color-theme;
+        font-size:0;
+        .text{
+            display inline-block;
+            vertical-align top;
+            line-height: 44px;
+            font-size: $font-size-large;
+        }
+        .icon {
+            display:inline-block;
+            vertical-align:top;
+            margin-top: 6px;
+            width: 30px;
+            height: 32px;
+            margin-right: 9px;
+            bg-image('logo');
+            background-size: 30px 32px;
+        }
+    }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import store from './store'
-
+import "normalize.css"
+import "@/common/stylus/index.styl"
+import fastclick from 'fastclick'
 Vue.config.productionTip = false
-
+fastclick.attach(document.body)
 new Vue({
   router,
   store,
